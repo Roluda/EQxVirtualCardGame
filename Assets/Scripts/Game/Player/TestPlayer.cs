52591a9564a4daecf09f65c9c47b464a8efd5a24
Robin Daraban <robin.daraban@gmx.de>
2020-11-25 16:26:45 +0100

    Add test player script

---
+++ Assets/Scripts/Game/Player/TestPlayer.cs
@@ -0,0 +1,27 @@
+﻿using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Generated;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EQx.Game.Player {
+    public class TestPlayer : TestPlayerBehavior {
+
+        public override void jump(RpcArgs args) {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (networkObject.IsOwner) {
+                networkObject.rotation = transform.rotation;
+                networkObject.position = transform.position;
+            }
+        }
+    }
+}
