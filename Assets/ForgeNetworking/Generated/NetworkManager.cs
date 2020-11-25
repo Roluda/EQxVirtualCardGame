ccfcee7b6c05377a6b34b359d1d8e80c58b5db31
Robin Daraban <robin.daraban@gmx.de>
2020-11-25 15:35:30 +0100

    Rename BeardedManStudios to ForgeNetworking

---
+++ Assets/ForgeNetworking/Generated/NetworkManager.cs
@@ -0,0 +1,114 @@
+using BeardedManStudios.Forge.Networking.Generated;
+using System;
+using UnityEngine;
+
+namespace BeardedManStudios.Forge.Networking.Unity
+{
+	public partial class NetworkManager : MonoBehaviour
+	{
+		public delegate void InstantiateEvent(INetworkBehavior unityGameObject, NetworkObject obj);
+		public event InstantiateEvent objectInitialized;
+		protected BMSByte metadata = new BMSByte();
+
+		public GameObject[] testNetworkObject = null;
+
+		protected virtual void SetupObjectCreatedEvent()
+		{
+			Networker.objectCreated += CaptureObjects;
+		}
+
+		protected virtual void OnDestroy()
+		{
+		    if (Networker != null)
+				Networker.objectCreated -= CaptureObjects;
+		}
+		
+		private void CaptureObjects(NetworkObject obj)
+		{
+			if (obj.CreateCode < 0)
+				return;
+				
+		if (obj is TestNetworkObject)
+			{
+				MainThreadManager.Run(() =>
+				{
+					NetworkBehavior newObj = null;
+					if (!NetworkBehavior.skipAttachIds.TryGetValue(obj.NetworkId, out newObj))
+					{
+						if (testNetworkObject.Length > 0 && testNetworkObject[obj.CreateCode] != null)
+						{
+							var go = Instantiate(testNetworkObject[obj.CreateCode]);
+							newObj = go.GetComponent<TestBehavior>();
+						}
+					}
+
+					if (newObj == null)
+						return;
+						
+					newObj.Initialize(obj);
+
+                    objectInitialized?.Invoke(newObj, obj);
+                });
+			}
+		}
+
+		protected virtual void InitializedObject(INetworkBehavior behavior, NetworkObject obj)
+		{
+            objectInitialized?.Invoke(behavior, obj);
+
+            obj.pendingInitialized -= InitializedObject;
+		}
+
+		/// <summary>
+		/// Instantiate an instance of Test
+		/// </summary>
+		/// <returns>
+		/// A local instance of TestBehavior
+		/// </returns>
+		/// <param name="index">The index of the Test prefab in the NetworkManager to Instantiate</param>
+		/// <param name="position">Optional parameter which defines the position of the created GameObject</param>
+		/// <param name="rotation">Optional parameter which defines the rotation of the created GameObject</param>
+		/// <param name="sendTransform">Optional Parameter to send transform data to other connected clients on Instantiation</param>
+		public TestBehavior InstantiateTest(int index = 0, Vector3? position = null, Quaternion? rotation = null, bool sendTransform = true)
+		{
+			var go = Instantiate(testNetworkObject[index]);
+			var netBehavior = go.GetComponent<TestBehavior>();
+
+			NetworkObject obj = null;
+			if (!sendTransform && position == null && rotation == null)
+				obj = netBehavior.CreateNetworkObject(Networker, index);
+			else
+			{
+				metadata.Clear();
+
+				if (position == null && rotation == null)
+				{
+					byte transformFlags = 0x1 | 0x2;
+					ObjectMapper.Instance.MapBytes(metadata, transformFlags);
+					ObjectMapper.Instance.MapBytes(metadata, go.transform.position, go.transform.rotation);
+				}
+				else
+				{
+					byte transformFlags = 0x0;
+					transformFlags |= (byte)(position != null ? 0x1 : 0x0);
+					transformFlags |= (byte)(rotation != null ? 0x2 : 0x0);
+					ObjectMapper.Instance.MapBytes(metadata, transformFlags);
+
+					if (position != null)
+						ObjectMapper.Instance.MapBytes(metadata, position.Value);
+
+					if (rotation != null)
+						ObjectMapper.Instance.MapBytes(metadata, rotation.Value);
+				}
+
+				obj = netBehavior.CreateNetworkObject(Networker, index, metadata.CompressBytes());
+			}
+
+			go.GetComponent<TestBehavior>().networkObject = (TestNetworkObject)obj;
+
+			FinalizeInitialization(go, netBehavior, obj, position, rotation, sendTransform);
+			
+			return netBehavior;
+		}
+	}
+}
