9bb78c846f6fa6f7441f26611b3620ab08b85a49
Robin Daraban <robin.daraban@gmx.de>
2020-11-25 19:10:40 +0100

    Add table seater

---
+++ Assets/Scripts/Game/Table/TableSeater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+
+namespace EQx.Game.Table {
+    public class TableSeater : MonoBehaviour {
+        [SerializeField]
+        float spawnRadius = 4;
+        [SerializeField]
+        float spawnHeight = 1;
+        // Start is called before the first frame update
+        void Start() {
+            Vector2 spawnPoint = Random.insideUnitCircle * spawnRadius;
+            NetworkManager.Instance.InstantiateTestPlayer(); //0, new Vector3(spawnPoint.x, spawnHeight, spawnPoint.y), Quaternion.identity);
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
