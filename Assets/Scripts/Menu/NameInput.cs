a8561f62473ccb430db658f706e893950616e270
Robin Daraban <robin.daraban@gmx.de>
2020-11-26 13:34:23 +0100

    Add name Iput field

---
+++ Assets/Scripts/Menu/NameInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace EQx.Menu {
+    public class NameInput : MonoBehaviour {
+        [SerializeField]
+        TMP_InputField nameField = default;
+        [SerializeField]
+        string defaultName = default;
+
+        public void SetPlayerName(string name) {
+            PlayerPrefs.SetString(PlayerPrefKeys.PLAYERNAME, name);
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+            nameField.text = PlayerPrefs.GetString(PlayerPrefKeys.PLAYERNAME, defaultName);
+        }
+    }
+}
