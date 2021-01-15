aa2bf2c400136fa5d477fbd107b2bc53bdf36bd5
Robin Daraban <robin.daraban@gmx.de>
2021-01-15 11:15:27 +0100

    Add URL Request Feature

---
+++ Assets/Scripts/Menu/URLRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EQx.Menu {
+    public class URLRequest : MonoBehaviour {
+        [SerializeField]
+        string url = "https://elitequality.org/#HomeWhy";
+
+        public void OpenURl() {
+            Application.OpenURL(url);
+        }
+    }
+}
