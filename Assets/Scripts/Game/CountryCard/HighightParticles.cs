f06e50f0d0bf60daa49a54e418c33bb7f957b0f0
Robin Daraban <robin.daraban@gmx.de>
2021-02-10 12:36:22 +0100

    Make Cards more Affordable

---
+++ Assets/Scripts/Game/CountryCard/HighightParticles.cs
@@ -0,0 +1,19 @@
+﻿using EQx.Game.CountryCards;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighightParticles : CountryCardComponent
+{
+    [SerializeField]
+    ParticleSystem system = default;
+
+    protected override void CardAffordableListener(CountryCard card) {
+        system.Play();
+    }
+
+    protected override void CardUnaffordableListener(CountryCard card) {
+        Debug.Log("Stopping Affordance");
+        system.Stop();
+    }
+}
