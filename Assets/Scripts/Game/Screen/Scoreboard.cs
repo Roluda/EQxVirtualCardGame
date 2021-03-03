c12b6c12adbe5183571409f23bbe9688411722f6
Robin Daraban <robin.daraban@gmx.de>
2021-03-03 10:42:49 +0100

    Add Scoreboard scripts

---
+++ Assets/Scripts/Game/Screen/Scoreboard.cs
@@ -0,0 +1,45 @@
+﻿using EQx.Game.Player;
+using EQx.Game.Table;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EQx.Game.Screen {
+    public class Scoreboard : MonoBehaviour {
+        [SerializeField]
+        Transform context = default;
+        [SerializeField]
+        ScoreboardEntry entryPrefab = default;
+
+        List<ScoreboardEntry> entries = new List<ScoreboardEntry>();
+
+        private void Awake() {
+            RoundManager.instance.onPlayerRegister += AddEntry;
+            RoundManager.instance.onPlayerUnregister += RemoveEntry;
+        }
+
+        private void RemoveEntry(CardPlayer player) {
+            var removedEntry = entries.Where(entry => entry.observedPlayer == player).First();
+            entries.Remove(removedEntry);
+            Destroy(removedEntry.gameObject);
+        }
+
+        private void AddEntry(CardPlayer player) {
+            var entry = Instantiate(entryPrefab, context);
+            entry.Init(player);
+            entries.Add(entry);
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Screen/ScoreboardEntry.cs
@@ -0,0 +1,57 @@
+﻿using EQx.Game.Investing;
+using EQx.Game.Player;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EQx.Game.Screen {
+    public class ScoreboardEntry : MonoBehaviour {
+
+        [SerializeField]
+        Image icon = default;
+        [SerializeField]
+        TMP_Text nameText = default;
+        [SerializeField]
+        TMP_Text coinsText = default;
+        [SerializeField]
+        TMP_Text rankText = default;
+        [SerializeField]
+        TMP_Text vcpText = default;
+        [SerializeField]
+        string coinsPrefix = default;
+        [SerializeField]
+        string rankPrefix = default;
+        [SerializeField]
+        string vcpPrefix = default;
+
+        public CardPlayer observedPlayer;
+
+
+        private void OnEnable() {
+            InvestmentManager.instance.onCapitalUpdated += CapitalUpdatedListener;
+        }
+
+        private void OnDisable() {
+            InvestmentManager.instance.onCapitalUpdated -= CapitalUpdatedListener;
+        }
+
+        private void CapitalUpdatedListener(CardPlayer player) {
+            if (player == observedPlayer) {
+                int capital = InvestmentManager.instance.Capital(player);
+                int rank = InvestmentManager.instance.GetRank(player);
+                coinsText.text = $"{coinsPrefix}{capital}";
+                rankText.text = $"{rankPrefix}{rank}";
+            }
+        }
+
+        public void Init(CardPlayer player) {
+            observedPlayer = player;
+            nameText.text = player.playerName;
+            var sprites = Resources.LoadAll<Sprite>("Sprites/Characters");
+            icon.sprite = sprites[player.avatarID];
+        }
+    }
+}
